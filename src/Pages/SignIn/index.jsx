f69b9e6f312fc1f9a8f0870e4f7a5770e356b1b9
dcarminatti <diogo.ca03@gmail.com>
2024-01-19 19:01:45 -0300

    feat: created a login page and styled it

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,5 +1,28 @@
-import { Container } from "./style";
+import { FiLock, FiMail } from "react-icons/fi";
+
+import { Input } from "../../Components/Input";
+import { Button } from "../../Components/Button";
+import { ButtonText } from "../../Components/ButtonText";
+
+import { Container, Form } from "./style";
 
 export function SignIn() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Form>
+        <h1>Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis</p>
+
+        <h2>Faça seu login</h2>
+
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
+
+        <Input placeholder="Senha" type="password" icon={FiLock} />
+
+        <Button title="Entrar" />
+
+        <a>Criar conta</a>
+      </Form>
+    </Container>
+  );
 }
