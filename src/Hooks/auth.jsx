cb3728901d9a048c50ff248cda1d456923e49c7a
dcarminatti <diogo.ca03@gmail.com>
2024-02-03 11:20:40 -0300

    feat: create signOut system

--- src/Hooks/auth.jsx
+++ src/Hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { api } from "../Services/api";
 
 const AuthContext = createContext({});
@@ -11,7 +11,10 @@ function AuthProvider({ children }) {
       const response = await api.post("/sessions", { email, password }),
         { user, token } = response.data;
 
-      api.defaults.headers.authorization = `Bearer ${token}`;
+      localStorage.setItem("@notes:user", JSON.stringify(user));
+      localStorage.setItem("@notes:token", token);
+
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
       setData({ user, token });
     } catch (error) {
       if (error.response) {
@@ -22,8 +25,25 @@ function AuthProvider({ children }) {
     }
   }
 
+  function signOut() {
+    localStorage.removeItem("@notes:user");
+    localStorage.removeItem("@notes:token");
+
+    setData({});
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem("@notes:token"),
+      user = localStorage.getItem("@notes:user");
+
+    if (token && user) {
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      setData({ user: JSON.parse(user), token });
+    }
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ signIn, user: data.user }}>
+    <AuthContext.Provider value={{ signIn, user: data.user, signOut }}>
       {children}
     </AuthContext.Provider>
   );
