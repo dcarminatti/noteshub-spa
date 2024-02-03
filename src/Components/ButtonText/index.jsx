da113645e6aca3b29e0478e6251511a98ffaf2b1
dcarminatti <diogo.ca03@gmail.com>
2024-02-03 10:36:38 -0300

    fix: "React does not recognize the `isActive` prop on a DOM element", replace "isActive" to "$isActive"

--- src/Components/ButtonText/index.jsx
+++ src/Components/ButtonText/index.jsx
@@ -2,7 +2,7 @@ import { Container } from "./style";
 
 export function ButtonText({ title, isActive = false, ...rest }) {
   return (
-    <Container type="button" isActive={isActive}>
+    <Container type="button" $isActive={!!isActive}>
       {title}
     </Container>
   );
