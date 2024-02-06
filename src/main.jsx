9d65e8bfb7959bc424a1d77759bf679d8a36fe15
dcarminatti <diogo.ca03@gmail.com>
2024-02-06 02:05:45 -0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Routes } from "./Routes";
+import { Routes } from "/Routes";
 
-import GlobalStyle from "./Styles/global";
+import GlobalStyle from "/Styles/global";
 import { ThemeProvider } from "styled-components";
-import theme from "./Styles/theme";
+import theme from "/Styles/theme";
 
-import { AuthProvider } from "./Hooks/auth";
+import { AuthProvider } from "/Hooks/auth";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
